4267ff0204ced5f30aa3deb8d60665cbf966bb08
Daniel <danielrojo@gmail.com>
2022-08-26 11:35:10 +0200

    camera sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_samples/pages/beers/beers_page.dart';
 import 'package:flutter_samples/pages/calculator/calculator_page.dart';
@@ -5,7 +6,13 @@ import 'package:flutter_samples/pages/heroes/heroes_page.dart';
 
 import 'pages/apod/apod_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
   runApp(const MyApp());
 }
 
